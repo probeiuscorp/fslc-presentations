395a4623d96eb99f6a150ed2494576012f936ae9
Caleb Stimpson <caleb.stimpson@gmail.com>
2025-03-24 15:18:13 -0600

    Add anon helper for anonymous components

---
+++ algebraic-effects/anonymous.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+// For HMR, mark slides.mdx as a dependency so the dispose handler is run when slides.mdx changes
+import './slides.mdx';
+
+const components = new Map();
+// Preserve identity of anonymous function components
+export function anon(fn) {
+  const key = fn.name;
+  if(!key) {
+    throw new Error('Function must have name');
+  }
+
+  const Component = components.has(key)
+    ? components.get(key)
+    : (components.set(key, fn), fn);
+  return <Component />;
+}
+
+if(module.hot) {
+  module.hot.dispose(() => {
+    components.clear();
+  });
+}
