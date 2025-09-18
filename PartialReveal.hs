e278aa0ebae580f65debce811da250d99dafe0ef
Caleb Stimpson <caleb.stimpson@gmail.com>
2025-09-18 14:48:20 -0600

    Add description to PartialReveal.hs

--- PartialReveal.hs
+++ PartialReveal.hs
@@ -3,6 +3,41 @@
   build-depends: base, optparse-applicative
 -}
 
+{-|
+Useful when you're using markdown slides (such as with maaslalani/slides) and
+you want to partially reveal slides. Unfortunately the only way to do so is
+duplicating the same slide multiple times but with progressively more content.
+Instead of duplicating content in your hand-written source, feed it through
+this script, which will duplicate the slide whenever it sees:
+
+  <!-- pause -->
+
+Give it an input file that looks like so:
+
+  # Intro slide
+
+  ---
+
+  # Part 1
+  Start here.
+
+  <!-- pause -->
+
+  # Part 2
+  The second part.
+
+  <!-- pause -->
+
+  # Part 3
+  The last part.
+
+  ---
+
+  # Next slide
+
+You'll get back a presentation now with 5 slides.
+-}
+
 {-# LANGUAGE LambdaCase #-}
 
 import Options.Applicative
